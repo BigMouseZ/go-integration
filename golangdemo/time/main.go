2ef34ea7a1a8ed5a94c241bdbd904e7493749ef9
张刚 <1085558160@qq.com>
2020-06-30 20:29:33 +0800

    time

--- golangdemo/time/main.go
+++ golangdemo/time/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"time"
 
+	"go-integration/golangdemo/time/timetest"
+)
+
+// time包提供了时间的显示和测量用的函数。日历的计算采用的是公历。
+func main() {
+	var Sunday int
+	Sunday = int(time.Sunday)
+	fmt.Println(Sunday)
+	log.Println(time.Sunday)
+
+	fmt.Println(timetest.Sunday)
 }
---
+++ golangdemo/time/timetest/timetest.go
@@ -0,0 +1,31 @@
+package timetest
+
+type Weekday int
+
+const (
+	Sunday Weekday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
+var days = [...]string{
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+}
+
+// String returns the English name of the day ("Sunday", "Monday", ...).
+func (d Weekday) String() string {
+	if Sunday <= d && d <= Saturday {
+		return days[d]
+	}
+	return ""
+}
