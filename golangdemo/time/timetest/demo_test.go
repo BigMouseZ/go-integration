2c0ba9839ad5a693a5af505ac35ecfe62037cfda
张刚 <1085558160@qq.com>
2020-07-02 11:27:01 +0800

    timetestdemo

---
+++ golangdemo/time/timetest/demo_test.go
@@ -0,0 +1,15 @@
+package timetest
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDemoOne(t *testing.T) {
+
+	fmt.Println("测试用例")
+	// t.SkipNow()
+	t.Log("测试用例")
+	// t.Skip("测试用例")
+
+}
--- golangdemo/time/timetest/timetest.go
+++ golangdemo/time/timetest/timetest.go
@@ -1,32 +1,60 @@
 package timetest
 
-type Weekday int
-
-const (
-	Sunday Weekday = iota
-	Monday
-	Tuesday
-	Wednesday
-	Thursday
-	Friday
-	Saturday
+import (
+	"fmt"
+	"time"
 )
 
-var days = [...]string{
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
+func TestOne() {
+	timeTemplate1 := "2006-01-02 15:04:05" // 常规类型
+	timeTemplate2 := "20060102150405"      // 常规类型
+	fmt.Println(time.Now().Format(timeTemplate1))
+	fmt.Println(time.Now().Format(timeTemplate2))
+	// 获取毫秒
+	fmt.Println(time.Microsecond)
+
+	// 获取月
+	fmt.Println(time.Month(12))
+	// 获星期
+	fmt.Println(time.Weekday(1))
+	// 当前时间
+	fmt.Println(time.Now())
+	fmt.Println(time.Now().String())
+	fmt.Println(time.Now().Day())
+	// 当前时间-小时
+	fmt.Println(time.Now().Hour())
+	fmt.Println(time.Now().Minute())
+	fmt.Println(time.Now().Second())
+	fmt.Println(time.Now().Nanosecond())
+	// 当前时间unix时间戳since 1970 -1- 1
+	fmt.Println(time.Now().Unix())
+
+	// 当前时间unix时间戳(nanoseconds),since 1970 -1- 1,
+	fmt.Println(time.Now().UnixNano())
+
+	// 当前时间加三个小时
+	fmt.Println("当前时间加三个小时", time.Now().Add(time.Hour*3))
+	// 时间戳转化成时间
+	currentTime := time.Now().Unix()
+	tm := time.Unix(currentTime, 0)
+	fmt.Println(tm)
+
 }
 
-// String returns the English name of the day ("Sunday", "Monday", ...).
-func (d Weekday) String() string {
+func TestTwo() {
+	fmt.Println("hello")
+	// 表示多少时间之后，但是在取出channel内容之前不阻塞，后续程序可以继续执行
+	chana := time.After(time.Second * 5)
 
-	if Sunday <= d && d <= Saturday {
-		return days[d]
+	fmt.Println("World")
+	fmt.Printf("%v", chana)
+
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("timed out")
 	}
-	return ""
+	fmt.Println(time.Now().After(time.Now().Add(time.Hour)))
+	fmt.Println(time.Now().Before(time.Now().Add(time.Hour)))
+	fmt.Println("增加天数", time.Now().AddDate(1, 1, 2))
+
 }
