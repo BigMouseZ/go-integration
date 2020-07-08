af9985cdaa57174098082e9e36b3cddde3491c2d
张刚 <1085558160@qq.com>
2020-07-08 17:31:26 +0800

    cryptotest

---
+++ golangdemo/crypto/crypto_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func TestMd5(t *testing.T) {
+
+}
+
+func TestAES(t *testing.T) {
+
+}
+
+func TestCipher(t *testing.T) {
+
+}
+
+func TestDSA(t *testing.T) {
+
+}
+
+func TestRSA(t *testing.T) {
+
+}
+
+func TestRand(t *testing.T) {
+
+}
+
+func TestDES(t *testing.T) {
+
+}
+
+func TestSha1(t *testing.T) {
+
+}
+
+func TestSha256(t *testing.T) {
+
+}
+
+func TestSha512(t *testing.T) {
+
+}
