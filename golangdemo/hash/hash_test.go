4fd6f8ebaf03835644d756c7c192c84c7b5ee872
zhanggang <zhanggang@signalway.com.cn>
2020-08-14 20:46:06 +0800

    hashtest

---
+++ golangdemo/hash/hash_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func Test(t *testing.T) {
+	//hash包提供hash函数的接口。
+	/*	主要应用场景
+		•文件校验
+		•数字签名
+		•鉴权协议
+
+		Go语言支持
+
+
+		go crypto标准包包含了一些常用的哈希算法，例如md5、sha1、sha256、sha512等。以sha1算法为例，了解下go如何生成哈希值。
+	*/
+}
