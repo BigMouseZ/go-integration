2c2834b8fc3138133d81893b9168312b44b6f708
zhanggang <1085558160@qq.com>
2020-08-22 20:31:39 +0800

    channel_test

--- goroutine/channel_produce_consumer_test.go
+++ goroutine/channel_produce_consumer_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -13,8 +14,8 @@ import (
 
 //消费者：计算每个随机数的每个位的数字的和  123232=？
 
-type ch1 chan *item
-type ch2 chan *result
+var itemChan chan *item
+var resultChan chan *result
 
 // 1个生产者 20消费者
 type item struct {
@@ -23,15 +24,14 @@ type item struct {
 }
 
 type result struct {
-	id  int64
-	num int64
+	item *item
+	sum  int64
 }
 
 //生产者
-func producer() {
+func producer(ch chan *item) {
 	//1、生成随机数
 	var id int64
-	ch1 := make(chan *item)
 	for {
 		rand.Seed(time.Now().UnixNano())
 		ret := rand.Int63()
@@ -41,32 +41,66 @@ func producer() {
 			num: ret,
 		}
 		//2、把随机数发送到通道里面,测试
-		ch1 <- tem
+		ch <- tem
 	}
 
 }
 
 //消费者
-func consumer(ch1 chan *item) {
-	tem := <-ch1
-	println("消费了：", tem)
+func consumer(ch chan *item, resChan chan *result) {
+	for tem := range ch {
+		//
+		ret := calc(tem.num)
+		//构建result结构体
+		resObj := &result{
+			item: tem,
+			sum:  ret,
+		}
+		//结构体指针
+		resChan <- resObj
+	}
+
+}
+
+//计算一个数每个位的和
+
+func calc(num int64) int64 {
+	var sum int64
+	for num > 0 {
+		sum = sum + num%10
+		num = num / 10
+	}
+	return sum
+}
+
+func startWorker(n int, ch chan *item, resChan chan *result) {
+	for i := 0; i < n; i++ {
+		go consumer(ch, resChan)
+	}
+
 }
 
 //打印结果
-
-func printResult() {
+func printResult(resChan chan *result) {
+	for ret := range resChan {
+		fmt.Printf("id:%v,num:%v,sum:%v \n", ret.item.id, ret.item.num, ret.sum)
+		time.Sleep(time.Second)
+		//fmt.Println(ret.sum, ret.item)
+	}
 
 }
 
 func TestPC(t *testing.T) {
-	//go producer()
-	//go consumer()
+	itemChan = make(chan *item, 100)
+	resultChan = make(chan *result, 100)
+	go producer(itemChan)
+	startWorker(20, itemChan, resultChan)
 	//
 	////打印结果
-	//printResult()
+	printResult(resultChan)
 
 	//给rand加随机数种子实现每次执行都能产生真正的随机数
-	rand.Seed(time.Now().UnixNano())
+	/*rand.Seed(time.Now().UnixNano())
 	ret := rand.Intn(101) //[1,101]
-	println(ret)
+	println(ret)*/
 }
